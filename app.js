351bfc60a1afa89c8ef3e13a1b84b894ac604913
Muratbek Nassyrov <130246057+wordpress-developer01@users.noreply.github.com>
2024-11-27 17:36:40 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+let myAge = 25; // my age
+let earlyYears = 2; // early years
+earlyYears = earlyYears * 10.5; // calculation for early years
+
+let laterYears = myAge - 2; // my age - 2
+laterYears = laterYears * 4; // calculation for later years
+
+let myAgeInDogYears = earlyYears + laterYears; // sum of earlyYears and laterYears
+
+let myName = 'Ali'; // name
+
+let res = `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`; // string interpolation
+
+console.log(res);
+
+
+
+
+
+
